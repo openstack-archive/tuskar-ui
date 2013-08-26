433eb2d4d9bfab71783961a42d15b7d53b6bb86b
Tzu-Mainn Chen <tzumainn@redhat.com>
2013-08-26 11:54:41 -0400

    Removed copyright from header
    Change-Id: I75cba8faca7eec03cb385a8a5a3cd13a81e12b02
    Fixes: bug #1216949

--- tuskar_ui/test/test_data/exceptions.py
+++ tuskar_ui/test/test_data/exceptions.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Nebula, Inc.
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
