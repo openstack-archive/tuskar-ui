724730a9a9261024d2a177914590da225d572a2c
Radomir Dopieralski <openstack@sheep.art.pl>
2013-09-26 15:54:10 +0200

    Merge FormsetStep and TableStep.
    The FormsetStep is no longer needed, TableStep is used instead, with a
    special version of DataTable, FormsetDataTable, that apart from all the
    normal DataTable features also generates a Django Formset with all of
    its data, and displays it the same way the FormsetStep did.
    
    One issue is not addressed:
    
     * The user-provided data in the forms can be lost if that row gets
       updated by ajax autoupdate.
    
    Change-Id: I5264b484f61212c21102739b803d40d9d31ae2cc

--- tuskar_ui/tables.py
+++ tuskar_ui/tables.py
@@ -12,15 +12,47 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import itertools
+import logging
+import sys
+
 from django import forms
+from django import template
+from django.template import loader
 from django.utils import datastructures
 from django.utils import html
-
 from horizon import conf
 from horizon.tables import base as horizon_tables
 
 
+LOG = logging.getLogger(__name__)
 STRING_SEPARATOR = "__"
+
+
+# FIXME: Remove this class and use Row directly after it becomes easier to
+# extend it, see bug #1229677
+class BaseCell(horizon_tables.Cell):
+    """ Represents a single cell in the table. """
+    def __init__(self, datum, column, row, attrs=None, classes=None):
+        super(BaseCell, self).__init__(datum, None, column, row, attrs,
+                                        classes)
+        self.data = self.get_data(datum, column, row)
+
+    def get_data(self, datum, column, row):
+        """ Fetches the data to be displayed in this cell. """
+        table = row.table
+        if column.auto == "multi_select":
+            widget = forms.CheckboxInput(check_test=lambda value: False)
+            # Convert value to string to avoid accidental type conversion
+            data = widget.render('object_ids',
+                                 unicode(table.get_object_id(datum)))
+            table._data_cache[column][table.get_object_id(datum)] = data
+        elif column.auto == "actions":
+            data = table.render_row_actions(datum)
+            table._data_cache[column][table.get_object_id(datum)] = data
+        else:
+            data = column.get_data(datum)
+        return data
 
 
 # FIXME: Remove this class and use Row directly after it becomes easier to
@@ -43,8 +75,7 @@ class BaseRow(horizon_tables.Row):
             datum = self.datum
         cells = []
         for column in table.columns.values():
-            data = self.load_cell_data(column, datum)
-            cell = horizon_tables.Cell(datum, data, column, self)
+            cell = table._meta.cell_class(datum, column, self)
             cells.append((column.name or column.auto, cell))
         self.cells = datastructures.SortedDict(cells)
 
@@ -67,57 +98,155 @@ class BaseRow(horizon_tables.Row):
         if display_name:
             self.attrs['data-display'] = html.escape(display_name)
 
-    def load_cell_data(self, column, datum):
-        table = self.table
-        if column.auto == "multi_select":
-            widget = forms.CheckboxInput(check_test=lambda value: False)
-            # Convert value to string to avoid accidental type conversion
-            data = widget.render('object_ids',
-                                 unicode(table.get_object_id(datum)))
-            table._data_cache[column][table.get_object_id(datum)] = data
-        elif column.auto == "actions":
-            data = table.render_row_actions(datum)
-            table._data_cache[column][table.get_object_id(datum)] = data
+
+class FormsetCell(BaseCell):
+    """A DataTable cell that knows about its field from the fieldset."""
+
+    def __init__(self, *args, **kwargs):
+        super(FormsetCell, self).__init__(*args, **kwargs)
+        try:
+            self.field = (self.row.form or {})[self.column.name]
+        except KeyError:
+            self.field = None
         else:
-            data = column.get_data(datum)
+            if self.field.errors:
+                self.attrs['class'] = self.attrs.get('class', '') + ' error'
+
+
+class FormsetRow(BaseRow):
+    """A DataTable row that knows about its form from the fieldset."""
+
+    template_path = 'formset_table/_row.html'
+
+    def __init__(self, column, datum, form):
+        self.form = form
+        super(FormsetRow, self).__init__(column, datum)
+        if self.cells == []:
+            # We need to be able to handle empty rows, because there may
+            # be extra empty forms in a formset. The original DataTable breaks
+            # on this, because it sets self.cells to [], but later expects a
+            # SortedDict. We just fill self.cells with empty Cells.
+            cells = []
+            for column in self.table.columns.values():
+                cell = self.table._meta.cell_class(None, column, self)
+                cells.append((column.name or column.auto, cell))
+            self.cells = datastructures.SortedDict(cells)
+
+    def render(self):
+        return loader.render_to_string(self.template_path,
+            {"row": self, "form": self.form})
+
+
+class FormsetDataTableMixin(object):
+    """
+    A mixin for DataTable to support Django Formsets.
+
+    This works the same as the ``FormsetDataTable`` below, but can be used
+    to add to existing DataTable subclasses.
+    """
+    formset_class = None
+
+    def __init__(self, *args, **kwargs):
+        super(FormsetDataTableMixin, self).__init__(*args, **kwargs)
+        self._formset = None
+
+        # Override Meta settings, because we need custom Form and Cell classes,
+        # and also our own template.
+        self._meta.row_class = FormsetRow
+        self._meta.cell_class = FormsetCell
+        self._meta.template = 'formset_table/_table.html'
+
+    def get_required_columns(self):
+        """Lists names of columns that have required fields."""
+        required_columns = []
+        if self.formset_class:
+            empty_form = self.get_formset().empty_form
+            for column in self.columns.values():
+                field = empty_form.fields.get(column.name)
+                if field and field.required:
+                    required_columns.append(column.name)
+        return required_columns
+
+    def _get_formset_data(self):
+        """Formats the self.filtered_data in a way suitable for a formset."""
+        data = []
+        for datum in self.filtered_data:
+            form_data = {}
+            for column in self.columns.values():
+                value = column.get_data(datum)
+                form_data[column.name] = value
+            form_data['id'] = self.get_object_id(datum)
+            data.append(form_data)
         return data
 
+    def get_formset(self):
+        """
+        Provide the formset corresponding to this DataTable.
 
-class MultiselectRow(BaseRow):
-    """
-    A DataTable Row class that handles pre-selected multi-select checboxes.
+        Use this to validate the formset and to get the submitted data back.
+        """
+        if self._formset is None:
+            self._formset = self.formset_class(
+                self.request.POST or None,
+                initial=self._get_formset_data(),
+                prefix=self._meta.name)
+        return self._formset
 
-    It adds custom code to pre-fill the checkboxes in the multi-select column
-    according to provided values, so that the selections can be kept between
-    requests.
-    """
+    def get_empty_row(self):
+        """Return a row with no data, for adding at the end of the table."""
+        return self._meta.row_class(self, None, self.get_formset().empty_form)
 
-    def load_cell_data(self, column, datum):
-        table = self.table
-        if column.auto == "multi_select":
-            # multi_select fields in the table must be checked after
-            # a server action
-            # TODO(remove this ugly code and create proper TableFormWidget)
-            multi_select_values = []
-            if (getattr(table, 'request', False) and
-                    getattr(table.request, 'POST', False)):
-                multi_select_values = table.request.POST.getlist(
-                        self.table.multi_select_name)
+    def get_rows(self):
+        """
+        Return the row data for this table broken out by columns.
 
-            multi_select_values += getattr(table,
-                                           'active_multi_select_values',
-                                           [])
-
-            if unicode(table.get_object_id(datum)) in multi_select_values:
-                multi_select_value = lambda value: True
+        The row objects get an additional ``form`` parameter, with the
+        formset form corresponding to that row.
+        """
+        try:
+            rows = []
+            if self.formset_class is None:
+                formset = []
             else:
-                multi_select_value = lambda value: False
-            widget = forms.CheckboxInput(check_test=multi_select_value)
+                formset = self.get_formset()
+                formset.is_valid()
+            for datum, form in itertools.izip_longest(self.filtered_data,
+                                                        formset):
+                row = self._meta.row_class(self, datum, form)
+                if self.get_object_id(datum) == self.current_item_id:
+                    self.selected = True
+                    row.classes.append('current_selected')
+                rows.append(row)
+        except Exception:
+            # Exceptions can be swallowed at the template level here,
+            # re-raising as a TemplateSyntaxError makes them visible.
+            LOG.exception("Error while rendering table rows.")
+            exc_info = sys.exc_info()
+            raise template.TemplateSyntaxError, exc_info[1], exc_info[2]
+        return rows
 
-            # Convert value to string to avoid accidental type conversion
-            data = widget.render(self.table.multi_select_name,
-                                 unicode(table.get_object_id(datum)))
-            table._data_cache[column][table.get_object_id(datum)] = data
-        else:
-            data = super(MultiselectRow, self).load_cell_data(column, datum)
-        return data
+    def get_object_id(self, datum):
+        # We need to support ``None`` when there are more forms than data.
+        if datum is None:
+            return None
+        return super(FormsetDataTableMixin, self).get_object_id(datum)
+
+
+class FormsetDataTable(FormsetDataTableMixin, horizon_tables.DataTable):
+    """
+    A DataTable with support for Django Formsets.
+
+    Note that :attr:`~horizon.tables.DataTableOptions.row_class` and
+    :attr:`~horizon.tables.DataTaleOptions.cell_class` are overwritten in this
+    class, so setting them in ``Meta`` has no effect.
+
+    .. attribute:: formset_class
+
+        A classs made with :function:`~django.forms.formsets.formset_factory`
+        containing the definition of the formset to use with this data table.
+
+        The columns that are named the same as the formset fields will be
+        replaced with form widgets in the table. Any hidden fields from the
+        formset will also be included. The fields that are not hidden and
+        don't correspond to any column will not be included in the form.
+    """
