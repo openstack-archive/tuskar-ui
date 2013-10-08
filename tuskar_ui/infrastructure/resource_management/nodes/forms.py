0229b377434596f8be06baa55b6c99e8d757a9f1
Radomir Dopieralski <openstack@sheep.art.pl>
2013-10-08 11:33:49 +0200

    Allow multiple nodes in a rack
    Use a FormsetDataTable in Rack's create and edit workflows to allow
    creating or updating multiple nodes at once. This is a stopgap solution
    needed before we completely rework this workflow according to the new
    wireframes. There are two problems left:
    
    * The formset table doesn't fit in the Rack's create/edit popup, so the
      user has to scroll horizontally.
    * There is currently no way to update the existing nodes in nova
      baremetal, so editing the fields of existing nodes has no effect.
    
    Implements: blueprint rack-nodes-1-to-many
    Change-Id: I27f5e9df97af6ba49acf9702fac17a0a883c6930

---
+++ tuskar_ui/infrastructure/resource_management/nodes/forms.py
@@ -0,0 +1,65 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import django.forms
+from django.utils.translation import ugettext_lazy as _  # noqa
+
+import tuskar_ui.forms
+
+
+class NodeForm(django.forms.Form):
+    id = django.forms.IntegerField(required=False,
+        widget=django.forms.HiddenInput())
+
+    service_host = django.forms.CharField(label=_("Name"), required=True,
+        widget=django.forms.TextInput(attrs={'class': 'input input-mini'}))
+    mac_address = django.forms.CharField(label=_("MAC Address"),
+        widget=django.forms.TextInput(attrs={'class': 'input input-small'}),
+        required=True)
+
+    # Hardware Specifications
+    cpus = django.forms.IntegerField(label=_("CPUs"), required=True,
+        min_value=1, widget=tuskar_ui.forms.NumberInput(attrs={
+            'class': 'input number_input_slim',
+        }))
+    memory_mb = django.forms.IntegerField(label=_("Memory"),
+        required=True, min_value=1, widget=tuskar_ui.forms.NumberInput(attrs={
+            'class': 'input number_input_slim',
+        }))
+    local_gb = django.forms.IntegerField(label=_("Local Disk (GB)"),
+        min_value=1, widget=tuskar_ui.forms.NumberInput(attrs={
+            'class': 'input number_input_slim',
+        }), required=True)
+
+    # Power Management
+    pm_address = django.forms.GenericIPAddressField(
+        widget=django.forms.TextInput(attrs={'class': 'input input-small'}),
+        label=_("Power Management IP"), required=False)
+    pm_user = django.forms.CharField(label=_("Power Management User"),
+        widget=django.forms.TextInput(attrs={'class': 'input input-mini'}),
+        required=False)
+    pm_password = django.forms.CharField(label=_("Power Management Password"),
+        required=False, widget=django.forms.PasswordInput(render_value=False,
+            attrs={'class': 'input input-mini'}))
+
+    # Access
+    terminal_port = django.forms.IntegerField(label=_("Terminal Port"),
+        required=False, min_value=0, max_value=1024,
+        widget=tuskar_ui.forms.NumberInput(attrs={
+            'class': 'input number_input_slim',
+        }))
+
+
+NodeFormset = django.forms.formsets.formset_factory(NodeForm, extra=1,
+    can_delete=True)
