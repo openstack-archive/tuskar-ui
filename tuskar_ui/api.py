39b78d82272ea6391b81077003c375066b5299e7
Ladislav Smola <lsmola@redhat.com>
2013-10-15 13:32:35 +0000

    Cleaning node api
    cleanup job for Hongkong demo
    
    - optimizing obtaining of nova isntance
    - fixing join of baremetal with nova instance
    - fixing of several attributes
    
    tested on lab on real undercloud
    
    Change-Id: Ibfa503594f99f1132d9ef2493406bc3493acba7a

--- tuskar_ui/api.py
+++ tuskar_ui/api.py
@@ -190,33 +190,24 @@ class BaremetalNode(StringIdAPIResourceWrapper):
         node = cls(baremetalclient(request).get(node_id))
         node.request = request
 
-        # FIXME ugly, fix after demo, make abstraction of instance details
-        # this is realy not optimal, but i dont hve time do fix it now
-        instances, more = nova.server_list(
-            request,
-            search_opts={'paginate': True},
-            all_tenants=True)
-
-        instance_details = {}
-        for instance in instances:
-            id = (instance.
-                  _apiresource._info['OS-EXT-SRV-ATTR:hypervisor_hostname'])
-            instance_details[id] = instance
-
-        detail = instance_details.get(node_id)
+        # Nova instance detail info will be added to baremetal node
+        # attributes.
+        detail = nova.server_get(request, node._apiresource.instance_uuid) 
         if detail:
+            # If baremetal is provisioned, there is a nova instance.
             addresses = detail._apiresource.addresses.get('ctlplane')
             if addresses:
                 node.ip_address_other = (", "
                     .join([addr['addr'] for addr in addresses]))
-
             node.status = detail._apiresource._info['OS-EXT-STS:vm_state']
             node.power_management = ""
             if node.pm_user:
                 node.power_management = node.pm_user + "/********"
         else:
+            # If baremetal is unprovisioned, there is no nova instance.
             node.status = 'unprovisioned'
 
+        # Returning baremetal node containing nova instance info
         return node
 
     @classmethod
@@ -329,6 +320,10 @@ class Node(StringIdAPIResourceWrapper):
     @property
     def mac_address(self):
         return self.nova_baremetal_node_attribute('mac_address')
+
+    @property
+    def ip_address_other(self):
+        return self.nova_baremetal_node_attribute('ip_address_other')
 
     @property
     def pm_address(self):
