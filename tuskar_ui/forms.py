40513d3023c670a15d0b8747bd44de0c2320d0bb
Radomir Dopieralski <openstack@sheep.art.pl>
2013-10-24 11:51:34 +0200

    Use a form field for subnetworks that has validation
    This field does validation, but doesn't do normalization.
    For that we should use a library.
    
    Change-Id: Ib1f540ee088d4231f139501c34886d10f3ca6cf4

--- tuskar_ui/forms.py
+++ tuskar_ui/forms.py
@@ -1,5 +1,15 @@
-from django.forms import widgets
+from django import forms
+from django.utils.translation import ugettext_lazy as _  # noqa
+import netaddr
 
 
-class NumberInput(widgets.TextInput):
+class NumberInput(forms.widgets.TextInput):
     input_type = 'number'
+
+
+class NetworkField(forms.fields.Field):
+    def clean(self, value):
+        try:
+            return str(netaddr.IPNetwork(value, version=4))
+        except netaddr.AddrFormatError:
+            raise forms.ValidationError(_("Enter valid IPv4 network address."))
