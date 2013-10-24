e3ba0aa1b039e1604eeb15a14e9dc55dc3ee36d3
Radomir Dopieralski <openstack@sheep.art.pl>
2013-10-24 10:22:10 +0200

    MACField with validation
    Use a field for MAC addresses that does proper validation and
    normalization.
    
    Change-Id: I18f8be2cec8db8f41424f672b9e99f70bf136633

--- tuskar_ui/forms.py
+++ tuskar_ui/forms.py
@@ -1,5 +1,30 @@
-from django.forms import widgets
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django import forms
+from django.utils.translation import ugettext_lazy as _  # noqa
+import netaddr
 
 
-class NumberInput(widgets.TextInput):
+class NumberInput(forms.widgets.TextInput):
     input_type = 'number'
+
+
+class MACField(forms.fields.Field):
+    def clean(self, value):
+        try:
+            return str(netaddr.EUI(
+                value, version=48, dialect=netaddr.mac_unix)).upper()
+        except netaddr.AddrFormatError:
+            raise forms.ValidationError(_(u'Enter a valid MAC address.'))
