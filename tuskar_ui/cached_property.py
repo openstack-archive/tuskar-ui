4d2b9c630f27dc348cb70708ecac517bd948611f
Radomir Dopieralski <openstack@sheep.art.pl>
2013-10-25 11:19:31 +0200

    Refactor the cached api calls into @cached_property
    We have a lot of repeating code in the tuskar_ui.api that
    deals with caching the api calls from properties. I made a
    @cached_property decorator that handles that all in one place
    instead.
    
    Change-Id: I097a3e9017f64c69762fc5bdcb44396386be948a

---
+++ tuskar_ui/cached_property.py
@@ -0,0 +1,83 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# Copyright (c) 2013 by the Werkzeug Team, see AUTHORS for more details.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#     * Redistributions of source code must retain the above copyright
+#       notice, this list of conditions and the following disclaimer.
+#
+#     * Redistributions in binary form must reproduce the above
+#       copyright notice, this list of conditions and the following
+#       disclaimer in the documentation and/or other materials provided
+#       with the distribution.
+#
+#     * The names of the contributors may not be used to endorse or
+#       promote products derived from this software without specific
+#       prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+# OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+# LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+# FIXME: replace with horizon.utils.memoized.cached_property when it becomes
+# available (see https://review.openstack.org/53859)
+class cached_property(object):
+    """
+    Works like @property, but caches its value per instance.
+
+    Unlike the @memoized decorator, it doesn't keep a reference to the instance
+    of the object, and thus doesn't make that object stay in memory forever.
+
+    This code is lifted from:
+    https://github.com/mitsuhiko/werkzeug/blob/master/werkzeug/utils.py#L35
+
+    The class has to have a ``__dict__`` in order for this property to work.
+    """
+
+    # Implementation detail: this property is implemented as non-data
+    # descriptor.  Non-data descriptors are only invoked if there is
+    # no entry with the same name in the instance's __dict__.
+    # This allows us to completely get rid of the access function call
+    # overhead.  If one choses to invoke __get__ by hand the property
+    # will still work as expected because the lookup logic is replicated
+    # in __get__ for manual invocation.
+
+    def __init__(self, func, name=None, doc=None):
+        self.__name__ = name or func.__name__
+        self.__module__ = func.__module__
+        self.__doc__ = doc or func.__doc__
+        self.func = func
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        try:
+            value = obj.__dict__[self.__name__]
+        except KeyError:
+            value = self.func(obj)
+            obj.__dict__[self.__name__] = value
+        return value
