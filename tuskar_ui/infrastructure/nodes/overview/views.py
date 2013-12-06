d119eebe7a117a39c0d3cca2e41b046c7edd89c6
Radomir Dopieralski <openstack@sheep.art.pl>
2013-12-06 15:59:50 +0100

    A dialog for registering nodes
    A dialog for registering new nodes, as described by the wireframe.
    
    Implements: blueprint tripleo-node-registration-widget
    Change-Id: I6c4566677a3ba5c2de0c6994d30133e1fdd5a60d

--- tuskar_ui/infrastructure/nodes/overview/views.py
+++ tuskar_ui/infrastructure/nodes/overview/views.py
@@ -11,8 +11,28 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+from django.core.urlresolvers import reverse_lazy  # noqa
 from django.views import generic
+import horizon.forms
+
+from tuskar_ui.infrastructure.nodes import forms
 
 
 class IndexView(generic.TemplateView):
-    template_name = 'infrastructure/base.html'
+    template_name = 'infrastructure/nodes/overview/index.html'
+
+
+class RegisterView(horizon.forms.ModalFormView):
+    form_class = forms.NodeFormset
+    form_prefix = 'register_nodes'
+    template_name = 'infrastructure/nodes/overview/register.html'
+    success_url = reverse_lazy(
+        'horizon:infrastructure:nodes.overview:index')
+
+    def get_data(self):
+        return []
+
+    def get_form(self, form_class):
+        return form_class(self.request.POST or None,
+                          initial=self.get_data(),
+                          prefix=self.form_prefix)
