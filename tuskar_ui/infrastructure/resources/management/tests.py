c2785008b840aeb755933b5fb87510d52a2820e8
Jiri Tomasek <jtomasek@redhat.com>
2013-12-16 13:22:10 +0100

    List views for Nodes in Resources management
    Management Nodes, Resource nodes, Unallocated nodes, Archived nodes
    BaremetalNode.list() is currently used to get data for the lists. This
    should be modified once filtering by node type is available
    
    Change-Id: I8499469b2487486e488990658f4ed5a75ee6983b

---
+++ tuskar_ui/infrastructure/resources/management/tests.py
@@ -0,0 +1,60 @@
+# -*- coding: utf8 -*-
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.core import urlresolvers
+from django import http
+
+from mox import IsA  # noqa
+
+from tuskar_ui import api as tuskar
+from tuskar_ui.test import helpers as test
+
+INDEX_URL = urlresolvers.reverse('horizon:infrastructure:resources.management'
+                                 ':index')
+RESOURCES_OVERVIEW_URL = urlresolvers.reverse('horizon:infrastructure:'
+                                              'resources.overview:index')
+
+
+class ManagementNodesTests(test.BaseAdminViewTests):
+    def setUp(self):
+        super(ManagementNodesTests, self).setUp()
+
+    @test.create_stubs({
+        tuskar.BaremetalNode: ('list',),
+    })
+    def test_index(self):
+        management_nodes = self.baremetal_nodes.list()
+
+        tuskar.BaremetalNode.list(
+            IsA(http.HttpRequest)).AndReturn(management_nodes)
+        self.mox.ReplayAll()
+
+        res = self.client.get(INDEX_URL)
+        self.assertTemplateUsed(
+            res, 'infrastructure/resources.management/index.html')
+
+        self.assertItemsEqual(res.context['management_nodes_table'].data,
+                              management_nodes)
+
+    @test.create_stubs({
+        tuskar.BaremetalNode: ('list',),
+    })
+    def test_index_nodes_list_exception(self):
+        tuskar.BaremetalNode.list(
+            IsA(http.HttpRequest)).AndRaise(self.exceptions.tuskar)
+
+        self.mox.ReplayAll()
+
+        res = self.client.get(INDEX_URL)
+        self.assertRedirectsNoFollow(res, RESOURCES_OVERVIEW_URL)
---
+++ tuskar_ui/infrastructure/resources/resource/tests.py
@@ -0,0 +1,60 @@
+# -*- coding: utf8 -*-
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.core import urlresolvers
+from django import http
+
+from mox import IsA  # noqa
+
+from tuskar_ui import api as tuskar
+from tuskar_ui.test import helpers as test
+
+INDEX_URL = urlresolvers.reverse('horizon:infrastructure:resources.resource'
+                                 ':index')
+RESOURCES_OVERVIEW_URL = urlresolvers.reverse('horizon:infrastructure:'
+                                              'resources.overview:index')
+
+
+class ResourceNodesTests(test.BaseAdminViewTests):
+    def setUp(self):
+        super(ResourceNodesTests, self).setUp()
+
+    @test.create_stubs({
+        tuskar.BaremetalNode: ('list',),
+    })
+    def test_index(self):
+        resource_nodes = self.baremetal_nodes.list()
+
+        tuskar.BaremetalNode.list(
+            IsA(http.HttpRequest)).AndReturn(resource_nodes)
+        self.mox.ReplayAll()
+
+        res = self.client.get(INDEX_URL)
+        self.assertTemplateUsed(
+            res, 'infrastructure/resources.resource/index.html')
+
+        self.assertItemsEqual(res.context['resource_nodes_table'].data,
+                              resource_nodes)
+
+    @test.create_stubs({
+        tuskar.BaremetalNode: ('list',),
+    })
+    def test_index_nodes_list_exception(self):
+        tuskar.BaremetalNode.list(
+            IsA(http.HttpRequest)).AndRaise(self.exceptions.tuskar)
+
+        self.mox.ReplayAll()
+
+        res = self.client.get(INDEX_URL)
+        self.assertRedirectsNoFollow(res, RESOURCES_OVERVIEW_URL)
