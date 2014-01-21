fded74fcda8a469d4d28260c350813ed1b898bff
Tzu-Mainn Chen <tzumainn@redhat.com>
2014-01-21 10:26:34 -0500

    Added initial tuskar api tests
    Change-Id: Ic00e55746edae3625f4f13e5c27e94d27e2a0d03

--- tuskar_ui/test/api_tests/tuskar_tests.py
+++ tuskar_ui/test/api_tests/tuskar_tests.py
@@ -14,14 +14,172 @@
 
 from __future__ import absolute_import
 
-# TODO(Tzu-Mainn Chen): uncomment once we add API tests
-#from tuskar_ui import api
+from glanceclient.v1 import images
+from heatclient.v1 import stacks
+
+from tuskar_ui import api
 from tuskar_ui.test import helpers as test
+
+# TODO(Tzu-Mainn Chen): uncomment mock data and mock
+# api calls once api.py stops using mock data
 
 
 class TuskarAPITests(test.APITestCase):
 
-    def setUp(self):
-        super(TuskarAPITests, self).setUp()
+    def test_overcloud_create(self):
+        #overcloud = self.tuskarclient_overclouds.first()
 
-    # to be written!
+        ret_val = api.Overcloud.create(self.request, [])
+        self.assertIsInstance(ret_val, api.Overcloud)
+
+    def test_overcloud_list(self):
+        #overclouds = self.tuskarclient_overclouds.list()
+
+        ret_val = api.Overcloud.list(self.request)
+        for oc in ret_val:
+            self.assertIsInstance(oc, api.Overcloud)
+        self.assertEqual(1, len(ret_val))
+
+    def test_overcloud_get(self):
+        overcloud = self.tuskarclient_overclouds.first()
+
+        ret_val = api.Overcloud.get(self.request, overcloud['id'])
+        self.assertIsInstance(ret_val, api.Overcloud)
+
+    def test_overcloud_stack(self):
+        overcloud = self.tuskarclient_overclouds.first()
+
+        ret_val = api.Overcloud(overcloud).stack
+        self.assertIsInstance(ret_val, stacks.Stack)
+
+    def test_overcloud_is_deployed(self):
+        overcloud = self.tuskarclient_overclouds.first()
+
+        ret_val = api.Overcloud(overcloud).is_deployed
+        self.assertFalse(ret_val)
+
+    def test_overcloud_resources(self):
+        overcloud = self.tuskarclient_overclouds.first()
+        category = self.tuskarclient_resource_categories.first()
+
+        ret_val = api.Overcloud(overcloud).resources(
+            api.ResourceCategory(category))
+        for i in ret_val:
+            self.assertIsInstance(i, api.Resource)
+        self.assertEqual(1, len(ret_val))
+
+    def test_overcloud_instances(self):
+        overcloud = self.tuskarclient_overclouds.first()
+        category = self.tuskarclient_resource_categories.first()
+
+        ret_val = api.Overcloud(overcloud).instances(
+            api.ResourceCategory(category))
+        for i in ret_val:
+            self.assertIsInstance(i, api.Instance)
+        self.assertEqual(1, len(ret_val))
+
+    def test_instance_get(self):
+        server = self.novaclient_servers.first()
+
+        ret_val = api.Instance.get(self.request, server.id)
+        self.assertIsInstance(ret_val, api.Instance)
+
+    def test_instance_list(self):
+        #servers = self.novaclient_servers.list()
+
+        ret_val = api.Instance.list(self.request)
+        for i in ret_val:
+            self.assertIsInstance(i, api.Instance)
+        self.assertEqual(4, len(ret_val))
+
+    def test_instance_node(self):
+        server = self.novaclient_servers.first()
+
+        ret_val = api.Instance(server).node
+        self.assertIsInstance(ret_val, api.Node)
+
+    def test_node_create(self):
+        node = self.ironicclient_nodes.first()
+
+        ret_val = api.Node.create(
+            self.request,
+            node.driver_info['ipmi_address'],
+            node.properties['cpu'],
+            node.properties['ram'],
+            node.properties['local_disk'],
+            ['aa:aa:aa:aa:aa:aa'],
+            ipmi_username='admin',
+            ipmi_password='password')
+
+        self.assertIsInstance(ret_val, api.Node)
+
+    def test_node_get(self):
+        node = self.ironicclient_nodes.first()
+
+        ret_val = api.Node.get(self.request, node.uuid)
+        self.assertIsInstance(ret_val, api.Node)
+
+    def test_node_get_by_instance_uuid(self):
+        node = self.ironicclient_nodes.first()
+
+        ret_val = api.Node.get(self.request, node.instance_uuid)
+        self.assertIsInstance(ret_val, api.Node)
+
+    def test_node_list(self):
+        #nodes = self.tuskarclient_overclouds.list()
+
+        ret_val = api.Node.list(self.request)
+        for node in ret_val:
+            self.assertIsInstance(node, api.Node)
+        self.assertEqual(5, len(ret_val))
+
+    def test_node_delete(self):
+        node = self.ironicclient_nodes.first()
+
+        api.Node.delete(self.request, node.uuid)
+
+    def test_node_addresses(self):
+        node = self.ironicclient_nodes.first()
+
+        ret_val = api.Node(node).addresses
+        self.assertEqual(2, len(ret_val))
+
+    def test_resource_get(self):
+        overcloud = self.tuskarclient_overclouds.first()
+        resource = self.heatclient_resources.first()
+
+        ret_val = api.Resource.get(self.request, overcloud,
+                                   resource.resource_name)
+        self.assertIsInstance(ret_val, api.Resource)
+
+    def test_resource_instance(self):
+        resource = self.heatclient_resources.first()
+
+        ret_val = api.Resource(resource).instance
+        self.assertIsInstance(ret_val, api.Instance)
+
+    def test_resource_node(self):
+        resource = self.heatclient_resources.first()
+
+        ret_val = api.Resource(resource).node
+        self.assertIsInstance(ret_val, api.Node)
+
+    def test_resource_category_list(self):
+        #categories = self.tuskarclient_resource_categories.list()
+
+        ret_val = api.ResourceCategory.list(self.request)
+        for c in ret_val:
+            self.assertIsInstance(c, api.ResourceCategory)
+        self.assertEqual(4, len(ret_val))
+
+    def test_resource_category_get(self):
+        category = self.tuskarclient_resource_categories.first()
+
+        ret_val = api.ResourceCategory.get(self.request, category['id'])
+        self.assertIsInstance(ret_val, api.ResourceCategory)
+
+    def test_resource_category_image(self):
+        category = self.tuskarclient_resource_categories.first()
+
+        ret_val = api.ResourceCategory(category).image
+        self.assertIsInstance(ret_val, images.Image)
