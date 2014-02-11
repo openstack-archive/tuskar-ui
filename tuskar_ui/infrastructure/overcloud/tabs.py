cce7b02da3b8c7bf26db67a4bbbad87eeabfea25
Radomir Dopieralski <openstack@sheep.art.pl>
2014-02-11 09:25:19 -0500

    Implement the deployment overview and progress page
    Work in progress.
    
    Change-Id: I280c9a9859e818f9e60dab22604a19844b741ff4
    Implements: blueprint tripleo-deployment-progress-page

--- tuskar_ui/infrastructure/overcloud/tabs.py
+++ tuskar_ui/infrastructure/overcloud/tabs.py
@@ -15,6 +15,7 @@
 #    under the License.
 
 from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ungettext_lazy
 
 from horizon import exceptions
 from horizon import tabs
@@ -23,47 +24,66 @@ from tuskar_ui import api
 from tuskar_ui.infrastructure.overcloud import tables
 
 
+def _get_role_data(overcloud, role):
+    resources = overcloud.resources(role, with_joins=True)
+    nodes = [r.node for r in resources]
+    node_count = len(nodes)
+    data = {
+        'role': role,
+        'name': role.name,
+        'node_count': node_count,
+    }
+    if nodes:
+        running_node_count = sum(1 for node in nodes
+                                 if node.instance.status == 'ACTIVE')
+        error_node_count = sum(1 for node in nodes
+                               if node.instance.status == 'ERROR')
+        deploying_node_count = (node_count - error_node_count -
+                                running_node_count)
+        data.update({
+            'running_node_count': running_node_count,
+            'error_node_count': error_node_count,
+            'error_node_message': ungettext_lazy("node is down",
+                                                 "nodes are down",
+                                                 error_node_count),
+            'deploying_node_count': deploying_node_count,
+            'deploying_node_message': ungettext_lazy("node is deploying",
+                                                     "nodes are deploying",
+                                                     deploying_node_count),
+        })
+        # TODO(rdopieralski) get this from ceilometer
+        # data['capacity'] = 20
+    return data
+
+
 class OverviewTab(tabs.Tab):
     name = _("Overview")
     slug = "overview"
     template_name = ("infrastructure/overcloud/_detail_overview.html")
 
-    def get_context_data(self, request):
-        context = {}
+    def get_context_data(self, request, **kwargs):
         overcloud = self.tab_group.kwargs['overcloud']
-
         try:
             roles = api.OvercloudRole.list(request)
         except Exception:
-            roles = {}
+            roles = []
             exceptions.handle(request,
-                              _('Unable to retrieve overcloud roles.'))
-
-        context['overcloud'] = overcloud
-        context['roles'] = []
-        for role in roles:
-            context['roles'].append(
-                self._get_role_data(overcloud, role))
-
-        # also get expected node counts
-        return context
-
-    def _get_role_data(self, overcloud, role):
-        resources = overcloud.resources(role, with_joins=True)
-        nodes = [r.node for r in resources]
-
-        role.node_count = len(nodes)
-        if role.node_count > 0:
-            role.running_node_count = len(
-                [n for n in nodes if n.instance.status == 'ACTIVE'])
-            role.error_node_count = len(
-                [n for n in nodes if n.instance.status == 'ERROR'])
-            role.other_node_count = role.node_count - \
-                (role.running_node_count +
-                 role.error_node_count)
-            role.running_node_percentage = 100 * \
-                role.running_node_count / role.node_count
-        return role
+                              _("Unable to retrieve overcloud roles."))
+        role_data = [_get_role_data(overcloud, role) for role in roles]
+        progress = (100 * sum(d.get('running_node_count', 0)
+                              for d in role_data) //
+                    sum(d['node_count'] for d in role_data))
+        try:
+            last_event = overcloud.stack_events[-1]
+        except IndexError:
+            last_event = None
+        return {
+            'overcloud': overcloud,
+            'roles': role_data,
+            'progress': progress,
+            'dashboard_url': overcloud.dashboard_url,
+            'last_event': last_event,
+        }
 
 
 class ConfigurationTab(tabs.Tab):
