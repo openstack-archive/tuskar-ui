9fb200407a0a25d1289a0e10360393354a9186e7
Radomir Dopieralski <openstack@sheep.art.pl>
2014-02-13 02:14:36 -0500

    Update the deployment design page
    Add the missing features and make it follow the latest
    wireframes.
    
    Change-Id: Ieeb100f25ec40ab8bc260ea542f098019d5cd04b
    Implements: blueprint tripleo-ui-deployment-design

--- tuskar_ui/infrastructure/overcloud/views.py
+++ tuskar_ui/infrastructure/overcloud/views.py
@@ -17,15 +17,48 @@ from django.utils.translation import ugettext_lazy as _
 from django.views.generic import base as base_views
 
 from horizon import exceptions
+import horizon.forms
 from horizon import tables as horizon_tables
 from horizon import tabs as horizon_tabs
 from horizon.utils import memoized
 import horizon.workflows
 
 from tuskar_ui import api
+from tuskar_ui.infrastructure.overcloud import forms
 from tuskar_ui.infrastructure.overcloud import tables
 from tuskar_ui.infrastructure.overcloud import tabs
 from tuskar_ui.infrastructure.overcloud.workflows import undeployed
+
+
+class OvercloudMixin(object):
+    @memoized.memoized
+    def get_overcloud(self):
+        overcloud_id = self.kwargs['overcloud_id']
+
+        try:
+            overcloud = api.Overcloud.get(self.request, overcloud_id)
+        except Exception:
+            msg = _("Unable to retrieve deployment.")
+            redirect = reverse('horizon:infrastructure:overcloud:index')
+            exceptions.handle(self.request, msg, redirect=redirect)
+
+        return overcloud
+
+
+class OvercloudRoleMixin(object):
+    @memoized.memoized
+    def get_role(self, overcloud):
+        role_id = self.kwargs['role_id']
+
+        try:
+            role = api.OvercloudRole.get(self.request, role_id)
+        except Exception:
+            msg = _("Unable to retrieve overcloud role.")
+            redirect = reverse('horizon:infrastructure:overcloud:detail',
+                               args=(overcloud.id,))
+            exceptions.handle(self.request, msg, redirect=redirect)
+
+        return role
 
 
 class IndexView(base_views.RedirectView):
@@ -50,22 +83,12 @@ class CreateView(horizon.workflows.WorkflowView):
     template_name = 'infrastructure/_fullscreen_workflow_base.html'
 
 
-class DetailView(horizon_tabs.TabView):
+class DetailView(horizon_tabs.TabView, OvercloudMixin):
     tab_group_class = tabs.DetailTabs
     template_name = 'infrastructure/overcloud/detail.html'
 
-    @memoized.memoized_method
-    def get_data(self):
-        overcloud_id = self.kwargs['overcloud_id']
-        try:
-            return api.Overcloud.get(self.request, overcloud_id)
-        except Exception:
-            msg = _("Unable to retrieve deployment.")
-            redirect = reverse('horizon:infrastructure:overcloud:index')
-            exceptions.handle(self.request, msg, redirect=redirect)
-
     def get_tabs(self, request, **kwargs):
-        overcloud = self.get_data()
+        overcloud = self.get_overcloud()
         return self.tab_group_class(request, overcloud=overcloud, **kwargs)
 
     def get_context_data(self, **kwargs):
@@ -74,21 +97,27 @@ class DetailView(horizon_tabs.TabView):
         return context
 
 
-class OvercloudRoleView(horizon_tables.DataTableView):
+class OvercloudRoleView(horizon_tables.DataTableView,
+                        OvercloudRoleMixin, OvercloudMixin):
     table_class = tables.OvercloudRoleNodeTable
     template_name = 'infrastructure/overcloud/overcloud_role.html'
 
+    @memoized.memoized
+    def _get_nodes(self, overcloud, role):
+        resources = overcloud.resources(role, with_joins=True)
+        return [r.node for r in resources]
+
     def get_data(self):
-        overcloud = self._get_overcloud()
-        role = self._get_role(overcloud)
+        overcloud = self.get_overcloud()
+        role = self.get_role(overcloud)
 
         return self._get_nodes(overcloud, role)
 
     def get_context_data(self, **kwargs):
         context = super(OvercloudRoleView, self).get_context_data(**kwargs)
 
-        overcloud = self._get_overcloud()
-        role = self._get_role(overcloud)
+        overcloud = self.get_overcloud()
+        role = self.get_role(overcloud)
 
         context['role'] = role
         context['image_name'] = role.image_name
@@ -96,34 +125,16 @@ class OvercloudRoleView(horizon_tables.DataTableView):
 
         return context
 
-    @memoized.memoized
-    def _get_nodes(self, overcloud, role):
-        resources = overcloud.resources(role, with_joins=True)
-        return [r.node for r in resources]
 
-    @memoized.memoized
-    def _get_overcloud(self):
-        overcloud_id = self.kwargs['overcloud_id']
+class OvercloudRoleEdit(horizon.forms.ModalFormView, OvercloudRoleMixin):
+    form_class = forms.OvercloudRoleForm
+    success_url = 'horizon:infrastructure:overcloud:create'
+    template_name = 'infrastructure/overcloud/role_edit.html'
 
-        try:
-            overcloud = api.Overcloud.get(self.request, overcloud_id)
-        except Exception:
-            msg = _("Unable to retrieve deployment.")
-            redirect = reverse('horizon:infrastructure:overcloud:index')
-            exceptions.handle(self.request, msg, redirect=redirect)
-
-        return overcloud
-
-    @memoized.memoized
-    def _get_role(self, overcloud):
-        role_id = self.kwargs['role_id']
-
-        try:
-            role = api.OvercloudRole.get(self.request, role_id)
-        except Exception:
-            msg = _("Unable to retrieve overcloud role.")
-            redirect = reverse('horizon:infrastructure:overcloud:detail',
-                               args=(overcloud.id,))
-            exceptions.handle(self.request, msg, redirect=redirect)
-
-        return role
+    def get_form_kwargs(self):
+        role = self.get_role(1)
+        return {
+            'initial': {
+                'id': role.id,
+            }
+        }
