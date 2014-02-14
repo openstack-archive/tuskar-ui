738af4ae777fd608dceda53e40a492b0c90ad757
Dmitry Tantsur <dtantsur@redhat.com>
2014-02-14 06:28:07 -0500

    Add CRUD operations for node profiles
    Add separate tab with list of all node profiles
    and buttons to create/edit/delete them.
    
    Node profiles implementation is based on Nova flavor
    management code in Horizon itself.
    
    TODO:
    - CRUD for Architecture field
    - Tests on edition and deletion of node profiles
    - User strings polishment
    
    Change-Id: I4041a7f59ab9a0abdcb66c7ae4d752284b472ad9

--- tuskar_ui/infrastructure/profiles/panel.py
+++ tuskar_ui/infrastructure/profiles/panel.py
@@ -1,4 +1,4 @@
-# vim: tabstop=4 shiftwidth=4 softtabstop=4
+# -*- coding: utf8 -*-
 #
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
@@ -16,25 +16,12 @@ from django.utils.translation import ugettext_lazy as _
 
 import horizon
 
-
-class BasePanels(horizon.PanelGroup):
-    slug = "infrastructure"
-    name = _("Infrastructure")
-    panels = (
-        'nodes',
-        'overcloud',
-        'profiles',
-    )
+from tuskar_ui.infrastructure import dashboard
 
 
-class Infrastructure(horizon.Dashboard):
-    name = _("Infrastructure")
-    slug = "infrastructure"
-    panels = (
-        BasePanels,
-    )
-    default_panel = 'overcloud'
-    permissions = ('openstack.roles.admin',)
+class Profiles(horizon.Panel):
+    name = _("Node Profiles")
+    slug = "profiles"
 
 
-horizon.register(Infrastructure)
+dashboard.Infrastructure.register(Profiles)
