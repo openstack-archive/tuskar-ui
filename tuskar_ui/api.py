ea0818cf4c0435bc35556befc9b92b32c5a4e148
Ladislav Smola <lsmola@redhat.com>
2014-02-18 13:06:09 +0100

    Dealing with Overcloud withou workflow
    -because Ocercloud will not be wrapping the whole
     create, upadte and delete workflow. We need to hack
     it around.
    
    Change-Id: Ieea997370c5efc888b4a87ec9455dcedbbbd8634

--- tuskar_ui/api.py
+++ tuskar_ui/api.py
@@ -13,21 +13,22 @@
 #    under the License.
 
 import logging
-
 import django.conf
 
+from horizon import exceptions
 from horizon.utils import memoized
+
+from novaclient.v1_1.contrib import baremetal
+from tuskarclient.v1 import client as tuskar_client
 
 from openstack_dashboard.api import base
 from openstack_dashboard.api import glance
 from openstack_dashboard.api import heat
 from openstack_dashboard.api import nova
-
-from novaclient.v1_1.contrib import baremetal
 from openstack_dashboard.test.test_data import utils
+
 from tuskar_ui.cached_property import cached_property  # noqa
 from tuskar_ui.test.test_data import tuskar_data
-from tuskarclient.v1 import client as tuskar_client
 
 LOG = logging.getLogger(__name__)
 TUSKAR_ENDPOINT_URL = getattr(django.conf.settings, 'TUSKAR_ENDPOINT_URL')
@@ -158,6 +159,30 @@ class Overcloud(base.APIResourceWrapper):
 
         return cls(overcloud, request=request)
 
+    # TODO(lsmola) before will will support multiple overclouds, we
+    # can work only with overcloud that is named overcloud. Delete
+    # this once we have more overclouds. Till then, this is the overcloud
+    # that rules them all.
+    # This is how API supports it now, so we have to have it this way.
+    # Also till Overcloud workflow is done properly, we have to work
+    # with situations that overcloud is deleted, but stack is still
+    # there. So overcloud will pretend to exist when stack exist.
+    @classmethod
+    def get_the_overcloud(cls, request):
+        overcloud_list = cls.list(request)
+        for overcloud in overcloud_list:
+            if overcloud.name == 'overcloud':
+                return overcloud
+
+        the_overcloud = cls(object(), request=request)
+        # I need to mock attributes of overcloud that is being deleted.
+        the_overcloud.id = "deleting_in_progress"
+
+        if the_overcloud.stack:
+            return the_overcloud
+        else:
+            raise exceptions.NotFound()
+
     @classmethod
     def delete(cls, request, overcloud_id):
         """Create an Overcloud in Tuskar
@@ -179,9 +204,17 @@ class Overcloud(base.APIResourceWrapper):
                  found
         :rtype:  heatclient.v1.stacks.Stack or None
         """
-        # FIXME(lsmola) load it properly, once the stack_is is filled
-        # properly in API
-        return heat.stack_get(self._request, 'overcloud')
+        # TODO(lsmola) load it properly, once the API has finished workflow
+        # and for example there can't be a situation when I delete Overcloud
+        # but Stack is still deleting. So the Overcloud will represent the
+        # state of all inner entities and operations correctly.
+        # Then also delete the try/except, it should not be caught on this
+        # level.
+        try:
+            return heat.stack_get(self._request, 'overcloud')
+        except Exception:
+            return None
+
 
     @cached_property
     def stack_events(self):
@@ -227,6 +260,15 @@ class Overcloud(base.APIResourceWrapper):
         """
         return self.stack.stack_status in ('CREATE_FAILED',
                                            'UPDATE_FAILED')
+
+    @cached_property
+    def is_deleting(self):
+        """Check if this Overcloud is deleting.
+
+        :return: True if Overcloud is deleting, False otherwise.
+        :rtype: bool
+        """
+        return self.stack.stack_status in ('DELETE_IN_PROGRESS', )
 
     @memoized.memoized
     def all_resources(self, with_joins=True):
