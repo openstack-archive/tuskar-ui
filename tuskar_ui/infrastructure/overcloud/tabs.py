08c0816e15964660d40d4a2b541a30c1b025330a
Ana Krivokapic <akrivoka@redhat.com>
2014-02-19 17:08:03 +0100

    Add pie charts to node overview and deployment overview
    Change-Id: Ic37bbad7b3520a8f3290bc7e412fb3ed5f35cbae

--- tuskar_ui/infrastructure/overcloud/tabs.py
+++ tuskar_ui/infrastructure/overcloud/tabs.py
@@ -77,12 +77,32 @@ class OverviewTab(tabs.Tab):
             last_event = overcloud.stack_events[-1]
         except IndexError:
             last_event = None
+
+        # TODO(akrivoka): Simplified version for now, only send controller
+        # and compute node counts to template, as pie chart cannot handle
+        # more anyway. Fix this when pie chart supports more than 2 values.
+
+        # TODO(akrivoka): Commenting out this part for now. We need better
+        # pie chart support in Horizon before this can be done.
+        # Relevant blueprint:
+        # https://blueprints.launchpad.net/horizon/+spec/piechart-enhancement
+
+        #controller_count = compute_count = 0
+        #for rd in role_data:
+        #    if rd['name'] == 'Controller':
+        #        controller_count = rd.get('running_node_count', 0)
+        #    elif rd['name'] == 'Compute':
+        #        compute_count = rd.get('running_node_count', 0)
+
         return {
             'overcloud': overcloud,
             'roles': role_data,
             'progress': progress,
             'dashboard_url': overcloud.dashboard_url,
             'last_event': last_event,
+            #'controller_count': controller_count,
+            #'compute_count': compute_count,
+            #'total_count': controller_count + compute_count,
         }
 
 
