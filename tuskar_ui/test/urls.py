5f0135c1bcb0302f9a8324640999e82e03ec47e2
Jordan OMara <jomara@redhat.com>
2014-04-10 12:52:58 -0400

    Fixing django conf url import, such as Bug: #1251248
    Change-Id: I8ecda3d31bf0c88435e3cc5da3b76f4dfe462232

--- tuskar_ui/test/urls.py
+++ tuskar_ui/test/urls.py
@@ -11,7 +11,7 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-from django.conf import urls
+from django.conf.urls import patterns, include, url
 from django.views import generic
 
 
