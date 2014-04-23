2796dfd72d509b82d26fd2d88cf955cc3fb92b29
Victor Sergeyev <vsergeyev@mirantis.com>
2014-04-23 13:47:18 +0300

    Replace Integer to Number in Type check
    There is no Integer type in Heat template, so we should use Number
    instead. See Heat parameters documentation
    http://docs.openstack.org/developer/heat/template_guide/hot_spec.html#hot-spec-parameters
    
    Add Type to ValueError message
    
    Change-Id: I74935c7c04cdd980ecc540f14baffbb4d115908c

--- tuskar_ui/infrastructure/overcloud/workflows/undeployed_configuration.py
+++ tuskar_ui/infrastructure/overcloud/workflows/undeployed_configuration.py
@@ -31,10 +31,10 @@ def make_field(name, Type, NoEcho, Default, Description, AllowedValues=None,
         attrs['placeholder'] = _("auto-generate")
     if Type == 'String':
         Field = django.forms.CharField
-    elif Type == 'Integer':
+    elif Type == 'Number':
         Field = django.forms.IntegerField
     else:
-        raise ValueError("Unsupported parameter type in Heat template.")
+        raise ValueError("Unsupported parameter type `%s` in Heat template." % Type)
     if NoEcho == 'true':
         Widget = django.forms.PasswordInput
         widget_kwargs['render_value'] = True
