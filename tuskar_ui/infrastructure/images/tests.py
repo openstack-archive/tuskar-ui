0fcdbb63a498376135ff363e8fd2b6ea939862d7
Ladislav Smola <lsmola@redhat.com>
2014-09-11 14:53:03 +0200

    Add images CRUD support
    Adding images CRUD support, importing code from Horizon.
    
    Change-Id: Ie054d33792881243201fdabed88aba2c72576e34

--- tuskar_ui/infrastructure/images/tests.py
+++ tuskar_ui/infrastructure/images/tests.py
@@ -13,16 +13,21 @@
 #    under the License.
 
 import contextlib
+import mock
+from mock import patch, call  # noqa
 
 from django.core import urlresolvers
 
-from mock import patch, call  # noqa
+from openstack_dashboard.dashboards.project.images.images import forms
 
 from tuskar_ui import api
 from tuskar_ui.test import helpers as test
 
 INDEX_URL = urlresolvers.reverse(
     'horizon:infrastructure:images:index')
+CREATE_URL = urlresolvers.reverse(
+    'horizon:infrastructure:images:create')
+UPDATE_URL = 'horizon:infrastructure:images:update'
 
 
 class ImagesTest(test.BaseAdminViewTests):
@@ -40,6 +45,125 @@ class ImagesTest(test.BaseAdminViewTests):
                   return_value=plans),
             patch('openstack_dashboard.api.glance.image_list_detailed',
                   return_value=[self.images.list(), False, False]),):
+
             res = self.client.get(INDEX_URL)
 
         self.assertTemplateUsed(res, 'infrastructure/images/index.html')
+
+    def test_create_get(self):
+        res = self.client.get(CREATE_URL)
+
+        self.assertTemplateUsed(res, 'infrastructure/images/create.html')
+
+    def test_update_get(self):
+        image = self.images.list()[0]
+
+        with contextlib.nested(
+            patch('openstack_dashboard.api.glance.image_get',
+                  return_value=image),) as (mocked_get,):
+
+            res = self.client.get(
+                urlresolvers.reverse(UPDATE_URL, args=(image.id,)))
+
+        mocked_get.assert_called_once_with(mock.ANY, image.id)
+        self.assertTemplateUsed(res, 'infrastructure/images/update.html')
+
+    def test_create_post(self):
+        image = self.images.list()[0]
+        data = {
+            'name': 'Fedora',
+            'description': 'Login with admin/admin',
+            'source_type': 'url',
+            'copy_from': 'http://test_url.com',
+            'disk_format': 'qcow2',
+            'architecture': 'x86-64',
+            'minimum_disk': 15,
+            'minimum_ram': 512,
+            'is_public': True,
+            'protected': False}
+
+        forms.IMAGE_FORMAT_CHOICES = [('qcow2', 'qcow2')]
+
+        with contextlib.nested(
+            patch('openstack_dashboard.api.glance.image_create',
+                  return_value=image),) as (mocked_create,):
+
+            res = self.client.post(CREATE_URL, data)
+
+        self.assertNoFormErrors(res)
+        self.assertEqual(res.status_code, 302)
+        self.assertRedirectsNoFollow(res, INDEX_URL)
+
+        mocked_create.assert_called_once_with(
+            mock.ANY, name=u'Fedora', container_format='bare', min_ram=512,
+            disk_format=u'qcow2', copy_from=u'http://test_url.com',
+            protected=False, min_disk=15, is_public=True,
+            properties={'description': u'Login with admin/admin',
+                        'architecture': u'x86-64'})
+
+    def test_update_post(self):
+        image = self.images.list()[0]
+        data = {
+            'image_id': image.id,
+            'name': 'Fedora',
+            'description': 'Login with admin/admin',
+            'source_type': 'url',
+            'copy_from': 'http://test_url.com',
+            'disk_format': 'qcow2',
+            'architecture': 'x86-64',
+            'minimum_disk': 15,
+            'minimum_ram': 512,
+            'is_public': True,
+            'protected': False}
+
+        forms.IMAGE_FORMAT_CHOICES = [('qcow2', 'qcow2')]
+
+        with contextlib.nested(
+            patch('openstack_dashboard.api.glance.image_get',
+                  return_value=image),
+            patch('openstack_dashboard.api.glance.image_update',
+                  return_value=image),) as (mocked_get, mocked_update,):
+
+            res = self.client.post(
+                urlresolvers.reverse(UPDATE_URL, args=(image.id,)), data)
+
+        self.assertNoFormErrors(res)
+        self.assertEqual(res.status_code, 302)
+        self.assertRedirectsNoFollow(res, INDEX_URL)
+
+        mocked_get.assert_called_once_with(mock.ANY, image.id)
+        mocked_update.assert_called_once_with(
+            mock.ANY, u'007e7d55-fe1e-4c5c-bf08-44b4a4964822', name=u'Fedora',
+            container_format='bare', disk_format=u'qcow2', protected=False,
+            purge_props=False, is_public=False,
+            properties={'description': u'Login with admin/admin',
+                        'architecture': u'x86-64'})
+
+    def test_delete_ok(self):
+        roles = [api.tuskar.Role(role) for role in
+                 self.tuskarclient_roles.list()]
+        plans = [api.tuskar.Plan(plan) for plan in
+                 self.tuskarclient_plans.list()]
+        images = self.images.list()
+
+        data = {'action': 'images__delete',
+                'object_ids': [images[0].id, images[1].id]}
+
+        with contextlib.nested(
+            patch('tuskar_ui.api.tuskar.Role.list',
+                  return_value=roles),
+            patch('tuskar_ui.api.tuskar.Plan.list',
+                  return_value=plans),
+            patch('openstack_dashboard.api.glance.image_list_detailed',
+                  return_value=[images, False, False]),
+            patch('openstack_dashboard.api.glance.image_delete',
+                  return_value=None),) as (
+                mock_role_list, plan_list, mock_image_lict, mock_image_delete):
+
+            res = self.client.post(INDEX_URL, data)
+
+        mock_image_delete.has_calls(
+            call(mock.ANY, images[0].id),
+            call(mock.ANY, images[1].id))
+        self.assertNoFormErrors(res)
+        self.assertRedirectsNoFollow(res, INDEX_URL)
