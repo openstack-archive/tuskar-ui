d2507fe58f8eb4ae23ae49576ef5629a63667485
Radomir Dopieralski <openstack@sheep.art.pl>
2014-09-30 12:34:38 +0200

    Automatic plan saving and validation
    Automatically save the plan and display the validation results
    on the plan overview page.
    
    Change-Id: Iab02ce69e58c6c0646b97e07787ab2382121a798

--- tuskar_ui/infrastructure/overview/views.py
+++ tuskar_ui/infrastructure/overview/views.py
@@ -11,8 +11,13 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
+
+import json
+
 from django.core.urlresolvers import reverse
 from django.core.urlresolvers import reverse_lazy
+from django import http
+from django.utils.translation import ugettext_lazy as _
 import heatclient
 import horizon.forms
 from horizon.utils import memoized
@@ -154,6 +159,37 @@ class IndexView(horizon.forms.ModalFormView, StackMixin):
             context['plan_invalid'] = any(message.get('is_critical')
                                           for message in messages)
         return context
+
+    def post(self, request, *args, **kwargs):
+        """If the post comes from ajax, return validation results as json."""
+
+        if not request.META.get('HTTP_X_HORIZON_VALIDATE', ''):
+            return super(IndexView, self).post(request, *args, **kwargs)
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        if form.is_valid():
+            handled = form.handle(self.request, form.cleaned_data)
+        else:
+            handled = False
+        if handled:
+            messages = forms.validate_plan(request, form.plan)
+        else:
+            # TODO(rdopieralski) Actually iterate over the form errors and
+            # add them all to the messages here.
+            messages = [{
+                'text': _(u"Error saving the plan."),
+                'is_critical': True,
+            }]
+        # We need to unlazify all the lazy urls and translations.
+        return http.HttpResponse(json.dumps({
+            'plan_invalid': any(m.get('is_critical') for m in messages),
+            'messages': [{
+                'text': unicode(m.get('text', '')),
+                'is_critical': m.get('is_critical', False),
+                'link_url': unicode(m.get('link_url', '')),
+                'link_label': unicode(m.get('link_label', '')),
+            } for m in messages],
+        }), mimetype='application/json')
 
 
 class DeployConfirmationView(horizon.forms.ModalFormView, StackMixin):
---
+++ tuskar_ui/infrastructure/static/infrastructure/js/tuskar.edit_plan.js
@@ -0,0 +1,68 @@
+tuskar.edit_plan = (function () {
+    'use strict';
+
+    var module = {};
+
+    module.debounce_timer = null;
+
+    module.init = function () {
+        // Attach event listeners and hide the submit button.
+        if (!$('form.deployment-roles-form')) {
+            return;
+        }
+        $('form.deployment-roles-form input.number-picker'
+            ).change(module.on_change);
+        $('form.deployment-roles-form [type=submit]').hide();
+        // Compile the templates.
+        module.message_template = Hogan.compile(
+            $('#message-template').html() || '');
+        module.title_template = Hogan.compile(
+            $('#title-template').html() || '');
+    };
+
+    module.on_change = function () {
+        // Only save when there was no activity for half a second.
+        window.clearTimeout(module.debounce_timer);
+        module.debounce_timer = window.setTimeout(module.save_form, 500);
+    };
+
+    module.save_form = function () {
+        // Save the current plan and get validation results.
+        var $form = $('form.deployment-roles-form');
+        module.update_messages(null);
+        $.ajax({
+          type: 'POST',
+          headers: {'X-Horizon-Validate': 'true'},
+          url: $form.attr('action'),
+          data: $form.serialize(),
+          dataType: 'json',
+          async: true,
+          success: module.update_messages,
+        });
+    };
+
+    module.update_messages = function (data) {
+        $('div.deployment-box ul').replaceWith(
+            module.message_template.render(data));
+        if (data === null) {
+            $('div.deployment-buttons a.btn-primary').addClass('disabled');
+            $('div.deployment-icon i').replaceWith(
+                '<i class="fa fa-spinner fa-spin text-info"></i>');
+        } else if (data.plan_invalid) {
+            $('div.deployment-buttons a.btn-primary').addClass('disabled');
+            $('div.deployment-icon i').replaceWith(
+                '<i class="fa fa-exclamation-circle text-danger"></i>');
+            $('div.deployment-box h4').replaceWith(
+                module.title_template.render(data));
+        } else {
+            $('div.deploymnet-buttons a.btn-primary').removeClass('disabled');
+            $('div.deployment-icon i').replaceWith(
+                '<i class="fa fa-check-circle text-success"></i>');
+            $('div.deployment-box h4').replaceWith(
+                module.title_template.render(data));
+        }
+    };
+
+    horizon.addInitFunction(module.init);
+    return module;
+} ());
