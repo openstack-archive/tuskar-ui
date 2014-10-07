cf689867bfc3098ec0a44dae938588decb27f77b
Radomir Dopieralski <openstack@sheep.art.pl>
2014-10-07 16:57:14 +0200

    New node registration and upload dialogs
    According to the new wireframes.
    
    Change-Id: I1c6430cc0165c7df612c85c31e8ad17d82870b5c

--- tuskar_ui/infrastructure/nodes/forms.py
+++ tuskar_ui/infrastructure/nodes/forms.py
@@ -11,7 +11,6 @@
 #    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 #    License for the specific language governing permissions and limitations
 #    under the License.
-
 import csv
 
 import django.forms
@@ -36,9 +35,7 @@ DRIVER_CHOICES = [
 
 def get_driver_info_dict(data):
     driver = data['driver']
-    driver_dict = {
-        'driver': driver
-    }
+    driver_dict = {'driver': driver}
     if driver == 'ipmi':
         driver_dict.update(
             # TODO(rdopieralski) If ipmi_address is no longer required,
@@ -56,18 +53,22 @@ def get_driver_info_dict(data):
     return driver_dict
 
 
-def auto_discover_node(request, kwargs):
-    node = api.node.Node.create(
-        request,
-        **kwargs
+def create_node(request, data):
+    kwargs = get_driver_info_dict(data)
+    kwargs.update(
+        cpu_arch=data.get('cpu_arch'),
+        cpus=data.get('cpus'),
+        memory_mb=data.get('memory_mb'),
+        local_gb=data.get('local_gb'),
+        mac_addresses=data['mac_addresses'].split(),
     )
-    api.node.Node.set_maintenance(request,
-                                  node.uuid,
-                                  True)
-    api.node.Node.discover(request, [node.uuid])
+    node = api.node.Node.create(request, **kwargs)
+    if data.get('do_autodiscovery', False):
+        api.node.Node.set_maintenance(request, node.uuid, True)
+        api.node.Node.discover(request, [node.uuid])
 
 
-class BaseNodeForm(django.forms.Form):
+class NodeForm(django.forms.Form):
     id = django.forms.IntegerField(
         label="",
         required=False,
@@ -139,6 +140,49 @@ class BaseNodeForm(django.forms.Form):
             'rows': 2,
         }),
     )
+    do_autodiscovery = django.forms.BooleanField(
+        label=_("Discover missing attributes"),
+        required=False,
+    )
+    mac_addresses = tuskar_ui.forms.MultiMACField(
+        label=_("NIC MAC Addresses"),
+        required=False,
+        widget=django.forms.Textarea(attrs={
+            'class': 'form-control',
+            'rows': '2',
+        }),
+    )
+    cpu_arch = django.forms.ChoiceField(
+        label=_("Architecture"),
+        required=False,
+        choices=CPU_ARCH_CHOICES,
+        widget=django.forms.Select(
+            attrs={'class': 'form-control'}),
+    )
+    cpus = django.forms.IntegerField(
+        label=_("CPUs"),
+        required=False,
+        min_value=1,
+        initial=1,
+        widget=tuskar_ui.forms.NumberInput(
+            attrs={'class': 'form-control'}),
+    )
+    memory_mb = django.forms.IntegerField(
+        label=_("Memory"),
+        required=False,
+        min_value=1,
+        initial=1,
+        widget=tuskar_ui.forms.NumberInput(
+            attrs={'class': 'form-control'}),
+    )
+    local_gb = django.forms.IntegerField(
+        label=_("Local Disk"),
+        required=False,
+        min_value=1,
+        initial=1,
+        widget=tuskar_ui.forms.NumberInput(
+            attrs={'class': 'form-control'}),
+    )
 
     def get_name(self):
         try:
@@ -149,22 +193,10 @@ class BaseNodeForm(django.forms.Form):
             name = _("Undefined node")
         return name
 
-    def create_node(self, request, data):
-        kwargs = get_driver_info_dict(data)
-        kwargs.update(
-            cpu_arch=data.get('cpu_arch'),
-            cpus=data.get('cpus'),
-            memory_mb=data.get('memory_mb'),
-            local_gb=data.get('local_gb'),
-            mac_addresses=data['mac_addresses'].split(),
-        )
-        api.node.Node.create(request, **kwargs)
-
     def handle(self, request, data):
         success = True
-        data = self.cleaned_data
         try:
-            self.create_node(request, data)
+            create_node(request, data)
         except Exception:
             success = False
             exceptions.handle(request, _('Unable to register node.'))
@@ -172,123 +204,65 @@ class BaseNodeForm(django.forms.Form):
             # have to unregister nodes, delete ports, etc?
         return success
 
+    def clean_ipmi_username(self):
+        return self.cleaned_data.get('ipmi_username') or None
 
-class RegisterNodeForm(BaseNodeForm):
-    mac_addresses = tuskar_ui.forms.MultiMACField(
-        label=_("NIC MAC Addresses"),
-        widget=django.forms.Textarea(attrs={
-            'class': 'form-control',
-            'rows': '2',
-        }),
-    )
-    cpu_arch = django.forms.ChoiceField(
-        label=_("Architecture"),
-        required=True,
-        choices=CPU_ARCH_CHOICES,
-        widget=django.forms.Select(
-            attrs={'class': 'form-control'}),
-    )
-    cpus = django.forms.IntegerField(
-        label=_("CPUs"),
-        required=True,
-        min_value=1,
-        initial=1,
-        widget=tuskar_ui.forms.NumberInput(
-            attrs={'class': 'form-control'}),
-    )
-    memory_mb = django.forms.IntegerField(
-        label=_("Memory"),
-        required=True,
-        min_value=1,
-        initial=1,
-        widget=tuskar_ui.forms.NumberInput(
-            attrs={'class': 'form-control'}),
-    )
-    local_gb = django.forms.IntegerField(
-        label=_("Local Disk"),
-        required=True,
-        min_value=1,
-        initial=1,
-        widget=tuskar_ui.forms.NumberInput(
-            attrs={'class': 'form-control'}),
-    )
+    def clean_ipmi_password(self):
+        return self.cleaned_data.get('ipmi_password') or None
 
-
-class AutoDiscoverNodeForm(BaseNodeForm):
-    mac_addresses = tuskar_ui.forms.MultiMACField(
-        label=_("NIC MAC Addresses"),
-        required=False,
-        widget=django.forms.Textarea(attrs={
-            'class': 'form-control switched',
-            'data-switch-on': 'driver',
-            'data-driver-pxe_ssh': _("PXE + SSH"),
-            'rows': 2,
-        }),
-    )
-
-    def create_node(self, request, data):
-        kwargs = get_driver_info_dict(data)
-        kwargs.update(
-            mac_addresses=data['mac_addresses'].split(),
-        )
-        auto_discover_node(request, kwargs)
+    def clean(self):
+        cleaned_data = super(NodeForm, self).clean()
+        if not cleaned_data.get('do_autodiscovery', False):
+            for field_name in [
+                'mac_addresses',
+                'cpu_arch',
+                'cpus',
+                'memory_mb',
+                'local_gb',
+            ]:
+                if not cleaned_data.get(field_name):
+                    self._errors[field_name] = self.error_class([(
+                        u"This field is required "
+                        u"when autodiscovery is disabled."
+                    )])
+        return cleaned_data
 
 
 class BaseNodeFormset(tuskar_ui.forms.SelfHandlingFormset):
-
     def clean(self):
         for form in self:
             if not form.cleaned_data:
                 raise django.forms.ValidationError(
                     _("Please provide node data for all nodes."))
-            if not form.cleaned_data.get('ipmi_username'):
-                form.cleaned_data['ipmi_username'] = None
-            if not form.cleaned_data.get('ipmi_password'):
-                form.cleaned_data['ipmi_password'] = None
+
+
+class UploadNodeForm(forms.SelfHandlingForm):
+    csv_file = forms.FileField(label=_("CSV File"), required=True)
+
+    def handle(self, request, data):
+        return True
+
+    def get_data(self):
+        data = []
+        for row in csv.reader(self.cleaned_data['csv_file']):
+            driver = row[0].strip()
+            if driver == 'pxe_ssh':
+                data.append(dict(
+                    ssh_address=row[1],
+                    ssh_username=row[2],
+                    ssh_key_contents=row[3],
+                    mac_addresses=row[4],
+                    driver=driver,
+                ))
+            elif driver == 'ipmi':
+                data.append(dict(
+                    ipmi_address=row[1],
+                    ipmi_username=row[2],
+                    ipmi_password=row[3],
+                    driver=driver,
+                ))
+        return data
 
 
 RegisterNodeFormset = django.forms.formsets.formset_factory(
-    RegisterNodeForm, extra=1,
-    formset=BaseNodeFormset)
-
-
-AutoDiscoverNodeFormset = django.forms.formsets.formset_factory(
-    AutoDiscoverNodeForm, extra=1,
-    formset=BaseNodeFormset)
-
-
-class AutoDiscoverCSVNodeForm(forms.SelfHandlingForm):
-    csv_file = forms.FileField(label=_("CSV File"),
-                               required=False)
-
-    def handle(self, request, data):
-        success = True
-        all_node_data = csv.reader(data['csv_file'])
-        for node_data in all_node_data:
-            driver = node_data[0]
-            kwargs = {
-                'driver': driver
-            }
-            if driver == 'pxe_ssh':
-                kwargs.update(
-                    ssh_address=node_data[1],
-                    ssh_username=node_data[2],
-                    ssh_key_contents=node_data[3],
-                    mac_addresses=node_data[4].split()
-                )
-            else:
-                kwargs.update(
-                    ipmi_address=node_data[1],
-                    ipmi_username=node_data[2],
-                    ipmi_password=node_data[3],
-                )
-
-            try:
-                auto_discover_node(request, kwargs)
-            except Exception:
-                success = False
-                exceptions.handle(request, _('Unable to register node.'))
-                # TODO(tzumainn) If there is a failure between steps, do we
-                # have to unregister nodes, delete ports, etc?
-
-        return success
+    NodeForm, extra=1, formset=BaseNodeFormset)
