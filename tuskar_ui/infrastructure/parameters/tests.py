05ae231a6ac7ce09cb378bd28493a90eaff0ae2b
Ladislav Smola <lsmola@redhat.com>
2014-10-08 12:39:18 +0200

    Update SNMP password parameter in all roles
    SNMP password needs to be updated in all roles, not in hardcoded
    list of roles.
    
    Change-Id: I4f6b822248d0eebc832d08b574afc48aa3b6bc13

--- tuskar_ui/infrastructure/parameters/tests.py
+++ tuskar_ui/infrastructure/parameters/tests.py
@@ -65,7 +65,8 @@ class ParametersTest(test.BaseAdminViewTests):
 
     def test_service_config_post(self):
         plan = api.tuskar.Plan(self.tuskarclient_plans.first())
-        role = api.tuskar.Role(self.tuskarclient_roles.first())
+        roles = [api.tuskar.Role(role) for role in
+                 self.tuskarclient_roles.list()]
         data = {
             'virt_type': 'qemu',
             'snmp_password': 'password',
@@ -76,7 +77,9 @@ class ParametersTest(test.BaseAdminViewTests):
             patch('tuskar_ui.api.tuskar.Plan.patch',
                   return_value=plan),
             patch('tuskar_ui.api.tuskar.Plan.get_role_by_name',
-                  return_value=role),
+                  return_value=roles[0]),
+            patch('tuskar_ui.api.tuskar.Plan.role_list',
+                  return_value=roles),
         ):
             res = self.client.post(SERVICE_CONFIG_URL, data)
             self.assertRedirectsNoFollow(res, INDEX_URL)
