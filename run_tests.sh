78c477b109a3e10055cd7eb87f7c22b6dce5992c
Lennart Regebro <lregebro@redhat.com>
2014-12-03 16:21:49 +0100

    Use eslint instead of jshint
    eslint has a plugin system which enables us to add openstack or horizon specific rules.
    
    Change-Id: I19932b50189e4e5e9ca938394787c5234f3d2322

--- run_tests.sh
+++ run_tests.sh
@@ -27,7 +27,7 @@ function usage {
   echo "  -p, --pep8               Just run pep8"
   echo "  -t, --tabs               Check for tab characters in files."
   echo "  -y, --pylint             Just run pylint"
-  echo "  -j, --jshint             Just run jshint"
+  echo "  -j, --jshint, --eslint   Just run eslint (Javascript lint)"
   echo "  -q, --quiet              Run non-interactively. (Relatively) quiet."
   echo "                           Implies -V if -N is not set."
   echo "  --only-selenium          Run only the Selenium unit tests"
@@ -65,7 +65,7 @@ just_pep8=0
 just_pylint=0
 just_docs=0
 just_tabs=0
-just_jshint=0
+just_eslint=0
 never_venv=0
 quiet=0
 restore_env=0
@@ -98,7 +98,7 @@ function process_option {
     -N|--no-virtual-env) always_venv=0; never_venv=1;;
     -p|--pep8) just_pep8=1;;
     -y|--pylint) just_pylint=1;;
-    -j|--jshint) just_jshint=1;;
+    -j|--jshint|--eslint) just_eslint=1;;
     -f|--force) force=1;;
     -t|--tabs) just_tabs=1;;
     -q|--quiet) quiet=1;;
@@ -143,9 +143,9 @@ function run_pylint {
   fi
 }
 
-function run_jshint {
-  echo "Running jshint ..."
-  jshint tuskar_ui/infrastructure/static/infrastructure
+function run_eslint {
+  echo "Running eslint ..."
+  eslint tuskar_ui/infrastructure/static/infrastructure
 }
 
 function run_pep8 {
@@ -453,9 +453,9 @@ if [ $just_tabs -eq 1 ]; then
     exit $?
 fi
 
-# Jshint
-if [ $just_jshint -eq 1 ]; then
-    run_jshint
+# JS Lint
+if [ $just_eslint -eq 1 ]; then
+    run_eslint
     exit $?
 fi
 
