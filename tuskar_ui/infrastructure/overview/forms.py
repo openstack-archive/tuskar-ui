21e00a9511c0900cdb4d55523602ce4a17459440
Giulio Fidente <gfidente@redhat.com>
2015-01-12 17:45:41 +0100

    Set NeutronL3HA to True when number of Controllers is > 1
    Change-Id: Ied3f9c49bec31af3d8af013f0240cd0af0b3b25d

--- tuskar_ui/infrastructure/overview/forms.py
+++ tuskar_ui/infrastructure/overview/forms.py
@@ -152,6 +152,10 @@ class EditPlan(horizon.forms.SelfHandlingForm):
             (field.role.node_count_parameter_name, data[name])
             for (name, field) in self.fields.items() if name.endswith('-count')
         )
+        controller_role = self.plan.get_role_by_name('controller')
+        if parameters[controller_role.node_count_parameter_name] > 1:
+            l3ha_param = controller_role.parameter_prefix + 'NeutronL3HA'
+            parameters[l3ha_param] = 'True'
         try:
             self.plan = self.plan.patch(request, self.plan.uuid, parameters)
         except Exception as e:
