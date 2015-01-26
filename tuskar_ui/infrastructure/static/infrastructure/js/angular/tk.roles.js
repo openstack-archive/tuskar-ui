bf1c83492f5dbaca8215498888a62b3b8e15beea
Jiri Tomasek <jtomasek@redhat.com>
2015-01-26 13:59:02 +0100

    WIP Angular roles list
    Change-Id: I3c8db050ad61e179d5b6991fde96f9505050a663

---
+++ tuskar_ui/infrastructure/static/infrastructure/js/angular/tk.roles.js
@@ -0,0 +1,20 @@
+(function() {
+  'use strict';
+
+  angular.module('tk.roles', [])
+    .controller('tkRolesCtrl', function($scope, $http) {
+
+      $scope.getRoles = function() {
+        $http.get('/api/tuskar/roles/')
+          .success(function(res) {
+            $scope.roles = res.items;
+          })
+          .error(function(){
+            horizon.alert('error', gettext('Unable to retrieve roles'));
+          });
+      }
+
+      $scope.getRoles();
+  });
+
+})();
