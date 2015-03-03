2b53f362e3006d95106b5fa76dcdb5883e0d1154
Ana Krivokapic <akrivoka@redhat.com>
2015-03-03 14:05:39 +0100

    Add IPMI metrics to node overview page
    Change-Id: Ibe39b7ba17004d9f6a2ec41eda7942b7ec487971

--- tuskar_ui/infrastructure/nodes/tabs.py
+++ tuskar_ui/infrastructure/nodes/tabs.py
@@ -142,6 +142,16 @@ class OverviewTab(tabs.Tab):
                  '100'),
             )
 
+        context['top_5'] = {
+            'fan': metering_utils.get_top_5(request, 'hardware.ipmi.fan'),
+            'voltage': metering_utils.get_top_5(
+                request, 'hardware.ipmi.voltage'),
+            'temperature': metering_utils.get_top_5(
+                request, 'hardware.ipmi.temperature'),
+            'current': metering_utils.get_top_5(
+                request, 'hardware.ipmi.current'),
+        }
+
         return context
 
 
